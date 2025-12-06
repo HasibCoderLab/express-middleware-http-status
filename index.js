a1f62bdd95efe82e9b93de839b0647aec6dc4595
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-06 14:41:15 +0600

    preview

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 import express from "express";
 const app = express();
-app.use(express.json())
-const port  = 8000;
+app.use(express.json());
+const port = 8000;
 
 let passwod = "1222"
-app.use((req,res,next) =>{
-if (req.body.pass != passwod) {
-    res.send("Password does not match ! ");
-}
-next()
-})
 
-app.post("/" , (req,res) =>{
+app.use((req, res, next) => {
+    if (req.body.pass != passwod) {
+        res.send("Password does not match");
+    }
+    next();
+});
+
+app.post("/", (req, res) => {
     console.log(req.body);
-    res.send({success:true});
-    
+    res.send({ success: true });
+
 })
 
-app.listen(port , () =>{
+app.listen(port, () => {
     console.log(`server is stated at ${port}`);
-    
+
 })
 
 
