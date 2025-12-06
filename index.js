e0b9328c43dcae0128029665d3ab5ae86b03fbb5
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-06 13:25:23 +0600

    fixet med middleware issue

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express();
+app.use(express.json())
+const port  = 8000;
+
+let passwod = "1222"
+app.use((req,res,next) =>{
+if (req.body.pass != passwod) {
+    res.send("Password does not match ! ");
+}
+next()
+})
+
+app.post("/" , (req,res) =>{
+    console.log(req.body);
+    res.send({success:true});
+    
+})
+
+app.listen(port , () =>{
+    console.log(`server is stated at ${port}`);
+    
+})
+
+
